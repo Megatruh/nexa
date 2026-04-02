4e93fcd6e0b0db4137076259160d52c6e91bd135
Shatramaja Parrot <farhaneshaputra132@gmail.com>
2026-04-02 16:18:07 +0700

    relasi hasmany di model subtest

--- app/Models/Subtest.php
+++ app/Models/Subtest.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subtest extends Model
 {
@@ -14,4 +15,12 @@ class Subtest extends Model
         'slug',
         'description',
     ];
+    public function questions():HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
+    public function learningMaterials():HasMany
+    {
+        return $this->hasMany(LearningMaterial::class);
+    }
 }

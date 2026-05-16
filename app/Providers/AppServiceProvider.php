b3a56d1d5ad2f9e1bc40eeb08b17b36fc9bfd038
Shatramaja Parrot <farhaneshaputra132@gmail.com>
2026-05-16 18:32:09 +0700

    add: buat report fitur tryout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+// Tambahkan import ini di atas
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        // Tambahkan baris sakti ini:
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
+        
+        // Atau kalau mau lebih pasti buat Ngrok, pakai ini saja:
+        if (str_contains(config('app.url'), 'https')) {
+            URL::forceScheme('https');
+        }
     }
 }

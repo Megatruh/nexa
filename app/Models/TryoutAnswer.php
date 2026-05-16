f9aa9ae59a4226e17861510078f4665adcb83f50
Shatramaja Parrot <farhaneshaputra132@gmail.com>
2026-05-16 23:30:40 +0700

    tampilkan hasil tryout

--- app/Models/TryoutAnswer.php
+++ app/Models/TryoutAnswer.php
@@ -8,10 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class TryoutAnswer extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'tryout_session_id',
         'tryout_question_id',
         'answer',
         'is_doubtful',
     ];
+
+    /**
+     * Relasi ke soal yang dijawab
+     */
+    public function question()
+    {
+        return $this->belongsTo(TryoutQuestion::class, 'tryout_question_id');
+    }
+
+    /**
+     * Relasi ke sesi tryout
+     */
+    public function session()
+    {
+        return $this->belongsTo(TryoutSession::class, 'tryout_session_id');
+    }
 }
